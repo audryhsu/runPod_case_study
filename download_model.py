718ca74345ece47afd279c0bb3a037872c58562e
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2025-01-28 04:51:48 +0000

    print all ENVIRONMENT VARIABLES

--- download_model.py
+++ download_model.py
@@ -1,8 +1,8 @@
 from huggingface_hub import snapshot_download
 import os
 
-print("Trying to find a secret:", os.getenv('RUNPOD_SECRET_HUGGINGFACE_TOKEN'))
-token = os.getenv("RUNPOD_SECRET_HUGGINGFACE_TOKEN")
+print("Trying to find all ENVIRONMENT VARIABLES:", os.environ)
+token = os.getenv("HUGGINGFACE_TOKEN")
 if not token:
     raise ValueError("HUGGINGFACE_TOKEN is not set in the environment.")
 # Path to download the model in the mounted volume
